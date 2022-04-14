8770b393b05a002e85333622698b6afbde65cf2e
Canhui Huang <huangcanhui121@gmail.com>
2022-04-14 15:55:00 -0600

    [add] chat

--- util/controls.py
+++ util/controls.py
@@ -8,14 +8,14 @@ import random
 def click(x, y, sleepTime=0.01):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(sleepTime)  # Pauses the script for 0.01 seconds.
+    time.sleep(sleepTime)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 
 def right_click(x, y, sleepTime=0.01):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
-    time.sleep(sleepTime)  # Pauses the script for 0.01 seconds.
+    time.sleep(sleepTime)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
 
 
@@ -23,6 +23,7 @@ def press_key(key_name):
     keyboard.press(key_name)
     time.sleep(random.randint(1, 10) / 100)
     keyboard.release(key_name)
+    time.sleep(0.1)
 
 
 def attackWalk(x, y, sleepTime=0.06):
@@ -31,3 +32,17 @@ def attackWalk(x, y, sleepTime=0.06):
     keyboard.release('a')
     time.sleep(0.06)
     click(x, y, sleepTime)
+
+
+def exitNotPressed():
+    return not (keyboard.is_pressed('ctrl+escape') or keyboard.is_pressed('ctrl+shift+q'))
+
+
+def typeInChat(text):
+    press_key('enter')
+    time.sleep(0.6)
+    for char in text:
+        press_key(char)
+    time.sleep(0.2)
+    press_key('enter')
+    time.sleep(0.2)
